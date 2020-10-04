996422bb17faa7c65ad5ba8ee98c4d60630f89c6
zyayun <331575886@163.com>
2020-10-04 23:46:46 +0800

    outline

--- flask-form.py
+++ flask-form.py
@@ -1,22 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 
 
-app=Flask(__name__)
-app.secret_key='hello'
+app = Flask(__name__)
+app.secret_key = "hello"
+
 
 class LoginForm(FlaskForm):
-    username = StringField('用户名:',description='用户名')
-    password = PasswordField('密码:',description='密码')
-    password2 = PasswordField('确认密码:',description='确认密码')
-    submit = SubmitField('提交')
+    username = StringField("用户名:", description="用户名")
+    password = PasswordField("密码:", description="密码")
+    password2 = PasswordField("确认密码:", description="确认密码")
+    submit = SubmitField("提交")
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm() 
-    return render_template('index.html',form=form)
+    form = LoginForm()
+    return render_template("index.html", form=form)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
